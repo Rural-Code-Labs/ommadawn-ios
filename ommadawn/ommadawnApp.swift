d4d57dfc4cfc4ebc8adce8a8e371372451e2873b
Rafael García Álvarez <vattenbit@gmail.com>
2026-07-23 19:13:40 +0200

    Initial Commit

---
+++ ommadawn/ommadawnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ommadawnApp.swift
+//  ommadawn
+//
+//  Created by Rafael García on 23/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ommadawnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ommadawnTests/ommadawnTests.swift
@@ -0,0 +1,18 @@
+//
+//  ommadawnTests.swift
+//  ommadawnTests
+//
+//  Created by Rafael García on 23/07/2026.
+//
+
+import Testing
+
+struct ommadawnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
